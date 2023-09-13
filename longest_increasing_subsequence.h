867053c34205eb98f8b82ae0b46ace4729ae4404
Virux Zheng <viruxzheng@gmail.com>
2023-09-13 21:31:42 +0800

    longest subsequence with diff one

--- longest_increasing_subsequence.h
+++ longest_increasing_subsequence.h
@@ -29,3 +29,25 @@ int longestIncreasingSubsequence(std::vector<int>& num){
     }
     return result;
 }
+/**
+ * Algorithm: longest subsequence with difference one.
+ * Type: DP
+ * Statement: Given an array A, find the longest subsequence
+ * such that difference between adjacent elemen is one.
+*/
+int longestSubsequenceWithDiffOne(std::vector<int>& num){
+    int n = num.size();
+    std::vector<int> dp(n, 0);
+    dp[0] = 1;
+    int result = 1;
+    for(int i = 1; i < n; i++){
+        dp[i] = 1;
+        for(int j = 0; j < i; j++){
+            if(std::abs(num[i] - num[j]) == 1){
+                dp[i] = std::max(dp[i], dp[j] + 1);
+            }
+        }
+        result = std::max(result, dp[i]);
+    }
+    return result;
+}
