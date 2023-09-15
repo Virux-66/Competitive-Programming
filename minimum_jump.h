0126903d6cb0f16346d1f92773604965f4728c6e
Virux Zheng <viruxzheng@gmail.com>
2023-09-15 22:17:16 +0800

    minimum jump

---
+++ minimum_jump.h
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+/**
+ * Algorithms: Minimum number of jumps to reach end
+ * Type: DP
+ * Statement: Given an positive integer N and a list of N integers A[]. 
+ * Each element in the array denotes the maximum length of jump you can cover. 
+ * Find out if you can make it to the last index if you start at the first index of the list.
+ * And if you can reach the end, find the minimum number of jumps.
+ * Analysis: This problem is different from stair climbing problem where one floor of a stair
+ * can either jump forward one or two step. In this problem, for any floor i, it's possible to
+ * reach from 0 to i-1. So we can't adpat the same algorithm to stair climbing problem.
+ * Time Complexity: O(N)
+*/
+int minimumJump(std::vector<int> nums){
+    int n = nums.size();
+    std::vector<int> dp(n + 1, 0);
+    int end = 0;
+    for(int i = 0; i < n; i++){
+        if(end >= n - 1){
+            break;
+        }
+        if(nums[i] == 0 && i == end){
+            return 0; // indicate non-reachable
+        }
+        if(i + nums[i] > end){
+            dp[end + 1] = dp[i] + 1;
+            end = i + nums[i];
+        }
+    }
+    return dp[n - 1];
+}
