9dff817b50c45fe8be86ff36db22f8261862d30d
Virux Zheng <viruxzheng@gmail.com>
2024-03-24 22:12:21 +0800

    segment tree with template

--- data_structure/segment_tree.h
+++ data_structure/segment_tree.h
@@ -5,47 +5,49 @@
  * Query: O(logN)
  * Modification: O(logN)
  */
-
+template <typename T>
 class TreeNode{
     public:
-        int val;
-        TreeNode* left;
-        TreeNode* right;
+        T val;
+        TreeNode<T>* left;
+        TreeNode<T>* right;
         int left_index;
         int right_index;
-        TreeNode():val(0), left(nullptr), right(nullptr){}
-        TreeNode(int v): val(v), left(nullptr), right(nullptr){}
+        TreeNode():left(nullptr), right(nullptr){}
+        TreeNode(T v): val(v), left(nullptr), right(nullptr){}
 };
 
-class segment_tree{
+template<typename T, typename Func>
+class SegmentTree{
     public:
-        TreeNode* root;
-        segment_tree(std::vector<int> vec){
+        TreeNode<T>* root_;
+        Func functor_;
+        SegmentTree(std::vector<T> vec){
             int n = vec.size();
-            root = build(vec, 0, n - 1);
+            root_ = Build(vec, 0, n - 1);
         }
 
-        TreeNode* build(std::vector<int>& vec, int left_index, int right_index){
+        TreeNode<T>* Build(std::vector<T>& vec, int left_index, int right_index){
             if(left_index == right_index){
-                TreeNode* root = new TreeNode(vec[left_index]);
+                TreeNode<T>* root = new TreeNode<T>(vec[left_index]);
                 root->left_index = root->right_index = left_index;
                 return root;
             }else{
-                TreeNode* root = new TreeNode();
-                root->left = build(vec, left_index, (left_index + right_index) / 2);
-                root->right = build(vec, (left_index + right_index) / 2 + 1, right_index);
-                root->val = root->left->val + root->right->val;
+                TreeNode<T>* root = new TreeNode<T>();
+                root->left = Build(vec, left_index, (left_index + right_index) / 2);
+                root->right = Build(vec, (left_index + right_index) / 2 + 1, right_index);
+                root->val = functor_(root->left->val, root->right->val);
                 root->left_index = left_index;
                 root->right_index = right_index;
                 return root;
             }
         }
 
-        int query(int left_index, int right_index){
-            return queryStub(root, left_index, right_index);
+        T Query(int left_index, int right_index){
+            return QueryStub(root_, left_index, right_index);
         }
 
-        int queryStub(TreeNode* root, int left_index, int right_index){
+        T QueryStub(TreeNode<T>* root, int left_index, int right_index){
             if(left_index > right_index){
                 return 0;
             }
@@ -56,20 +58,20 @@ class segment_tree{
             int mid = (root->left_index + root->right_index) / 2;
         
             if(right_index <= mid){
-                return queryStub(root->left, left_index, right_index);
+                return QueryStub(root->left, left_index, right_index);
             }else if(left_index >= mid + 1){
-                return queryStub(root->right, left_index, right_index);
+                return QueryStub(root->right, left_index, right_index);
             }else{
-                return queryStub(root->left, left_index, mid) + 
-                        queryStub(root->right, mid + 1, right_index);
+                return functor_(QueryStub(root->left, left_index, mid),
+                        QueryStub(root->right, mid + 1, right_index));
             }
         }
 
-        void update(int pos, int new_val){
-            updateStub(root, pos, new_val);
+        void Update(int pos, T new_val){
+            UpdateStub(root_, pos, new_val);
         }
         
-        void updateStub(TreeNode* root, int pos, int new_val){
+        void UpdateStub(TreeNode<T>* root, int pos, T new_val){
             if(root->left_index == root->right_index){
                 root->val = new_val;
                 return;
@@ -77,9 +79,9 @@ class segment_tree{
 
             int mid = (root->left_index + root->right_index) / 2;
             if(pos <= mid){
-                updateStub(root->left, pos, new_val);
+                UpdateStub(root->left, pos, new_val);
             }else{
-                updateStub(root->right, pos, new_val);
+                UpdateStub(root->right, pos, new_val);
             }
             root->val = root->left->val + root->right->val;
         }
